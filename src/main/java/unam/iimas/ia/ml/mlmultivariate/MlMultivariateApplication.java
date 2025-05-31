72eac372a17226710e57c9623c18adb75b7d2f1c
djanr2 <djanr2@gmail.com>
2025-05-31 14:31:55 -0600

    20250531: init

---
+++ src/main/java/unam/iimas/ia/ml/mlmultivariate/MlMultivariateApplication.java
@@ -0,0 +1,13 @@
+package unam.iimas.ia.ml.mlmultivariate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlMultivariateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MlMultivariateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/unam/iimas/ia/ml/mlmultivariate/MlMultivariateApplicationTests.java
@@ -0,0 +1,13 @@
+package unam.iimas.ia.ml.mlmultivariate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlMultivariateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
