5e5331aaeec7c7b23332c477ac16c6dff9123e56
djanr2 <djanr2@gmail.com>
2025-12-07 17:49:33 -0600

    20251207: code depuring

--- src/main/java/unam/iimas/ia/ml/mlmultivariate/ega/EGA.java
+++ src/main/java/unam/iimas/ia/ml/mlmultivariate/ega/EGA.java
@@ -6,6 +6,38 @@ import unam.iimas.ia.ml.mlmultivariate.file.LoadFile;
 
 import java.util.*;
 
+// TODO
+//  1. Abrir un archivo data .txt separado por tabs
+//      1.1 Separar las tuplas por TRN y TST aleatoriamente. Con un porcentaje de seleccion
+//      1.2 Mantener conjunto de datos completo en memoria (copia) para no tener que estar abriendo de nuevo el archivo.
+//      1.3 Del mismo archivo duplicar los datos
+//      1.4 Trabajar con las copias de los dattos y no con los originales.
+// TODO
+//  2. Establecer las clases individual, gen (->modelo), ega
+//      2.1 Establecer las propiedades (metadatos para correr el algoritmo EGA)
+//          2.1.1 numero de terminos
+//          2.1.2 potencia maxima l
+//          2.1.3 nombre/ubicacion del archivo -> determinar si el archivo tiene encabezados o hay que ponerlos
+//              2.1.3.1 Separar el archivo en TRN y TST en archivos para saber cuales fueron
+//              2.1.3.2 Archivos de cada proceso ? -> validar que archivos conviene imprimir
+//              2.1.3.3 Archivo de salida con resultado (modelo)
+//          *2.1.4 Quasi Minmax -> Se tiene que cambiar el AlgoritmoAscensoRapido
+//          2.1.5 Pc, Probabilidad de cruce siempre se cruzan.
+//          2.1.6 Pm, Probabilidad de mutacion valores del libro
+//          2.1.7 numero de individuos por generacion
+//          2.1.8 numero de generaciones
+//          2.1.9 Factor de regularizacion 0.00001 genberalmente (Esto esta en la clase Precision en RHO)
+//          *2.1.9 Ver si se establece el error RMS
+// TODO
+//  3. Establecer conexion con base de datos
+//      3.1 Generar tablas
+// TODO
+//  4. Establecer servicio API rest
+// TODO
+//  5. Diseñar el front-end
+// TODO
+//  6. Revisar el comparable de Vector
+
 public class EGA {
     private final int epoch = 100;
     private final double mutation_probability = 0.5;
@@ -31,7 +63,6 @@ public class EGA {
         //for (int i = 0; i < individuals.size(); i++) {
             //System.out.println(individuals.get(i).getBestEpsilonPhiValue()+ ": "+individuals.get(i).getModelo());
         //}
-
     }
 
     private List<AlgoritmoAscensoRapido> getNewGeneration(){
